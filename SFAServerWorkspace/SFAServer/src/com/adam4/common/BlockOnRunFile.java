c52ce4e85cb4dc92dcc7752acac510bb3a04e9bb
unknown <Cristian@Tatooine.Milkyway.Adam4.com>
2015-03-23 13:17:38 -0400

    updated file block

--- SFAServerWorkspace/SFAServer/src/com/adam4/common/BlockOnRunFile.java
+++ SFAServerWorkspace/SFAServer/src/com/adam4/common/BlockOnRunFile.java
@@ -1,86 +1,69 @@
 package com.adam4.common;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
 import com.adam4.mylogger.MyLogger.LogLevel;
 
-public class BlockOnRunFile
-{
-    private File watchedFile;
-    private Path path;
-    private WatchService watcher;
+public class BlockOnRunFile {
+    private final Path watchedFile;
 
-    public void end()
-    {
-        watchedFile.delete();
+    public BlockOnRunFile(String runFilePath) throws IOException {
+        watchedFile = Paths.get(runFilePath).toAbsolutePath();
+        Files.deleteIfExists(watchedFile);
+        // create entire directory tree, if possible, to create our watch file
+        // in.
+        Files.createDirectories(watchedFile.getParent());
+        Files.createFile(watchedFile);
+
     }
 
-    public BlockOnRunFile(String runFilePath) throws IOException
-    {
-        watcher = FileSystems.getDefault().newWatchService();
-        path = FileSystems.getDefault().getPath(runFilePath.substring(0, runFilePath.lastIndexOf(FileSystems.getDefault().getSeparator())));
-        watchedFile = new File(runFilePath);
-        watchedFile.createNewFile();
+    public void end() throws IOException {
+        Files.deleteIfExists(watchedFile);
     }
 
-    public void block()
-    {
-        try
-        {
-            WatchKey key;
-            key = path.register(watcher, StandardWatchEventKinds.ENTRY_DELETE);
+    public void block() {
+        try (WatchService watcher = FileSystems.getDefault().newWatchService()) {
+            final WatchKey key = watchedFile.getParent().register(watcher,
+                    StandardWatchEventKinds.ENTRY_DELETE);
+
             // stall until the game is supposed to end
             // reset key to allow new events to be detected
-            while (key.reset())
-            {
-                try
-                {
-                    for (WatchEvent<?> event : key.pollEvents())
-                    {
-                        WatchEvent.Kind<?> kind = event.kind();
-                        if (kind == StandardWatchEventKinds.OVERFLOW)
-                        {
-                            Common.log.logMessage("File watcher overflow", LogLevel.INFO);
-                            if (!watchedFile.exists())
-                            {
-                                // do nothing
-                                ;
-                            }
-                            break;
-                        }
-                        if (kind == StandardWatchEventKinds.ENTRY_DELETE)
-                        {
-                            @SuppressWarnings("unchecked")
-                            WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                            Path filename = ev.context();
-                            if (filename.toAbsolutePath().toString().equals(watchedFile.getAbsolutePath().toString()))
-                            {
-                                watcher.close();
-                                Common.log.logMessage("watchedFile file has been deleted", LogLevel.INFO);
-                                break;
-                            }
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    watcher.close();
-                    Common.log.logMessage(e, LogLevel.INFO);
-                    continue;
-                }
-            }// end while loop
-        }
-        catch (IOException e1)
-        {
+            while (key.reset()) {
 
+                // wait for a file to be deleted (or an overflow....)
+                if (key != watcher.take()) {
+                    throw new IllegalStateException(
+                            "Only our key is registered, only it should be taken");
+                }
+
+                // now, we know something has changed in the directory, all we
+                // care about though, is if our file exists.
+                if (!Files.exists(watchedFile)) {
+                    return;
+                }
+
+            }
+        } catch (IOException e1) {
             Common.log.logMessage(e1, LogLevel.ERROR);
+        } catch (InterruptedException e) {
+            // propagate an interrupt... we can't handle it here.....
+            // just let the file be removed, and we die....
+            Thread.currentThread().interrupt();
+            Common.log.logMessage(e, LogLevel.WARN);
+        } finally {
+            try {
+                Files.deleteIfExists(watchedFile);
+            } catch (IOException e) {
+                // unable to delete the sentry file.....
+                Common.log.logMessage(e, LogLevel.WARN);
+            }
         }
     }
 }
