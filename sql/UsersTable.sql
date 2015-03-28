16cd590589d80d79ae367ee5882862d06f243738
CrisAdam <CristianrAdam+GitHub@gmail.com>
2015-03-27 23:48:04 -0400

    fix quote

--- sql/UsersTable.sql
+++ sql/UsersTable.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `UsersTable` (
-  `userID` int(11) NOT NULL DEFAULT '100',
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(512) NOT NULL,
   `nickname` varchar(45) NOT NULL,
   `hashedpassword` varchar(1024) NOT NULL,
-  `salt` varchar(24) NOT NULL DEFAULT 'SELECT MD5(RAND());',
+  `salt` varchar(24) NOT NULL,
   `lastSuccessfulLogin` timestamp(4) NULL DEFAULT NULL,
   `accountCreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`userID`,`email`),
   UNIQUE KEY `userID_UNIQUE` (`userID`),
   UNIQUE KEY `username_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
