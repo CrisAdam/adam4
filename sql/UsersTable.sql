7326de5e00bffb9c494a1334d001844e7bfa4d48
CrisAdam <CristianrAdam+GitHub@gmail.com>
2015-03-29 17:26:49 -0400

    removed timestamp

--- sql/UsersTable.sql
+++ sql/UsersTable.sql
@@ -2,11 +2,10 @@ CREATE TABLE `UsersTable` (
   `userID` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(512) NOT NULL,
   `nickname` varchar(45) NOT NULL,
-  `hashedpassword` varchar(128) NOT NULL,
-  `salt` varchar(24) NOT NULL,
+  `hashedpassword` varchar(256) NOT NULL,
+  `salt` varchar(32) NOT NULL,
   `lastSuccessfulLogin` timestamp(4) NULL DEFAULT NULL,
   `accountCreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`userID`,`email`),
-  UNIQUE KEY `userID_UNIQUE` (`userID`),
-  UNIQUE KEY `username_UNIQUE` (`email`)
+  UNIQUE KEY `userID_UNIQUE` (`userID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
